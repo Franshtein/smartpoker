21b5c6999826cc99603eefbd9f2bf79162fcb707
Franshtein <vlad.stsetsevich@mail.ru>
2022-08-05 10:20:28 +0300

    add bd

--- src/main/java/com/stsetsevich/smartpoker/GreetingController.java
+++ src/main/java/com/stsetsevich/smartpoker/GreetingController.java
@@ -1,26 +1,61 @@
 package com.stsetsevich.smartpoker;
 
+import com.stsetsevich.smartpoker.domain.Message;
+import com.stsetsevich.smartpoker.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
 
-    @GetMapping
-    public String main(Map <String, Object> model)
-    {
-        model.put("some", "Visca el Barca!");
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("mesages", messages);
         return "main";
     }
 
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        //можно только одной строкой, вот так, только будет фильтровать и при пустом поле
+      //  List<Message> messages = messageRepo.findByTag(filter);
+
+        Iterable<Message> messages;
+
+        if(filter==null || filter.isEmpty())
+        {
+            messages = messageRepo.findAll();
+        }
+        else messages = messageRepo.findByTag(filter);
+        model.put("messages", messages);
+        return "main";
+
+    }
 }
