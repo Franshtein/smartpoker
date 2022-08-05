7b0de5818cf2056aa1167d550af101b2c8686b46
Franshtein <vlad.stsetsevich@mail.ru>
2022-08-05 12:53:13 +0300

    add bd

--- src/main/java/com/stsetsevich/smartpoker/controller/MainController.java
+++ src/main/java/com/stsetsevich/smartpoker/controller/MainController.java
@@ -1,19 +1,17 @@
-package com.stsetsevich.smartpoker;
+package com.stsetsevich.smartpoker.controller;
 
 import com.stsetsevich.smartpoker.domain.Message;
 import com.stsetsevich.smartpoker.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
---
+++ src/main/java/com/stsetsevich/smartpoker/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.stsetsevich.smartpoker.repos;
+
+import com.stsetsevich.smartpoker.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
