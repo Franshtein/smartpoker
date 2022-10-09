d2ea739ff43399a19825c2fec304d9d0d355b7e2
Franshtein <vlad.stsetsevich@mail.ru>
2022-10-09 11:01:26 +0300

    -добавлены комментарии, улучшена структура кода

--- src/main/java/com/stsetsevich/smartpoker/controller/HudController.java
+++ src/main/java/com/stsetsevich/smartpoker/controller/HudController.java
@@ -18,48 +18,37 @@ public class HudController {
     PlayerRepo playerRepo;
     @Autowired
     ExtraStatsCalc extraStatsCalc;
-
     @Autowired
     PlayersAtTable playersAtTable;
-
     @Autowired
     TableInfoCalc tableInfoCalc;
 
-
     @GetMapping("/hud")
-    public String hud(String nickname, Map<String, Object> model, String addPlayer, String addPlayer2, String addPlayer3
-            , String addPlayer4, String addPlayer5) {
+    public String hud(Map<String, Object> model) {
+        //первичная инициализация пустых таблиц
         playersAtTable.initPlayers();
         List<Player> pl = playersAtTable.getAllPlayers();
 
-
-
+        //добавляет в модель общую для всех методов в контроллере информацию
         modelPutGeneralInfo(model, pl);
 
         return "hud";
     }
 
-
+    //добавляет нового игрока за стол
     @PostMapping("addPlayer")
     public String addPlayer(@RequestParam String addPlayer, String addPlayer2, String addPlayer3
             , String addPlayer4, String addPlayer5, Map<String, Object> model) {
 
-
         List<Player> pl = playersAtTable.getAllPlayers(addPlayer, addPlayer2, addPlayer3, addPlayer4, addPlayer5);
         modelPutGeneralInfo(model, pl);
 
-
         return "hud";
-
     }
 
-
+    //Выводит всплывающее окно с дополнительными статами
     @PostMapping("addStat")
-    public String addStat(@RequestParam String addPlayer, String addPlayer2, String addPlayer3
-            , String addPlayer4, String addPlayer5, String addStat, String player, Map<String, Object> model) {
-        //можно только одной строкой, вот так, только будет фильтровать и при пустом поле
-        //  List<Message> messages = messageRepo.findByTag(filter);
-
+    public String extraStats(String addStat, String player, Map<String, Object> model) {
 
         List<Player> pl = playersAtTable.getAllPlayers();
         modelPutGeneralInfo(model, pl);
@@ -71,35 +60,37 @@ public class HudController {
         model.put("statPlayer", player);
         model.put("statValues", statInfos);
 
-
         return "hud";
 
     }
 
+    //добавляет в модель общую для всех методов информацию
     private void modelPutGeneralInfo(Map<String, Object> model, List<Player> pl) {
 
-
+        //Статы игроков для всех улиц
         model.put("plStats", playersAtTable.getPreflopStats());
         model.put("flopStats", playersAtTable.getFlopStats());
         model.put("turnStats", playersAtTable.getTurnStats());
         model.put("riverStats", playersAtTable.getRiverStats());
 
+        //Игроки
         model.put("player", pl.get(0));
         model.put("player2", pl.get(1));
         model.put("player3", pl.get(2));
         model.put("player4", pl.get(3));
         model.put("player5", pl.get(4));
 
+        //основная информация в правом верхнем углу
         model.put("tableinfo", tableInfoCalc.generalStatsCalc(pl));
 
-        //Информация о цвете таблиц
+        //Информация о цвете таблиц игроков
         model.put("seats", tableInfoCalc.hudSeatsColor(pl));
 
+        //Показать сообщение о возникшем исключении
         if (playersAtTable.getPlayerNotFoundException() != null) {
             model.put("playerError", playersAtTable.getPlayerNotFoundException());
             playersAtTable.setPlayerNotFoundException(null);
         }
-
     }
 
 
